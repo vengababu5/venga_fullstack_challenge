6a57c5e8bbd6df43a4affa7f76de836a1d132f88
vengababu5 <37792233+vengababu5@users.noreply.github.com>
2022-03-13 20:25:39 +0530

    news article changes

--- livechannel/src/NewsArticles/newsArticle.js
+++ livechannel/src/NewsArticles/newsArticle.js
@@ -13,27 +13,27 @@ import './newsArticle.css';
 const NewsArticles = () => {
     const [articles, setArticles] = useState([]);
     const [articleFilter, setArticalFilter] = useState('');
-    const [showRecords, setShowRecords]=useState(true);
+    const [showRecords, setShowRecords] = useState(true);
 
     //fetch article data
     const fetchArticlesData = () => {
         axios.get('http://localhost:3002/getnews')
-        .then(items => {
-            if(items?.data.length >0){
-                setArticles(items?.data);
-            } else {
-                setArticles([]);
+            .then(items => {
+                if (items ?.data.length > 0) {
+                    setArticles(items ?.data);
+                } else {
+                    setArticles([]);
+                    setShowRecords(false);
+                }
+            }).catch(err => {
                 setShowRecords(false);
-            }
-        }).catch(err=>{
-            setShowRecords(false);
-        });
+            });
     }
 
     //filter article data
     const filterArticlesData = (query) => {
         axios.get(`http://localhost:3002/filterarticle?q=${query}`).then(value => {
-            if (value?.data.length > 0) {
+            if (value ?.data.length > 0) {
                 let data = value.data.map(article => {
                     return { ...article, name: article.author };
                 })
@@ -41,17 +41,17 @@ const NewsArticles = () => {
             } else {
                 setArticles([]);
                 setShowRecords(false);
-            }  
+            }
         }).catch(err => {
             setShowRecords(false);
         });
     }
 
     //filter submit handler
-    const filterArticles = () =>{
-        if (articleFilter !==''){
+    const filterArticles = () => {
+        if (articleFilter !== '') {
             filterArticlesData(articleFilter);
-            
+
         }
     }
     useEffect(() => {
@@ -59,62 +59,45 @@ const NewsArticles = () => {
     }, [])
     return (
         <>
-            {articles.length ===0 ?
-                showRecords ? <Spinner  animation="border" size="lg" /> :'No records available'
-            :
-            <Container >
-                <Row>
-                    <Col md={6}>
-                        <h2>Articles</h2>
-                    </Col>
-                    <Col md={6} className="mt-2">
-                            <input type="text" placeholder="Enter value to filter" onChange={(e) => setArticalFilter(e.target.value)}  value={articleFilter} /> 
-                        <input type="button" value="Filter" onClick={filterArticles} />
-                    </Col>
-                </Row>               
-                <Row className="mt-2">
-                    {
-                        articles.length > 0 &&
-                        articles.map((article) => {
-                            return(
-                                <div className="article" key = {article.id}>
-                                    <Card>
-                                        <a href={article.url} target="_blank"><Card.Header>{article.name}</Card.Header>
-                                        </a>
-                                        <Card.Body>
-                                            <blockquote className="blockquote mb-0">
-                                                <p>
-                                                    {article.description}
-                                                </p>
-                                            </blockquote>
-                                        </Card.Body>
-                                    </Card>
-                                </div>
-                            )
-                        })
-                    }
-
-                <div>
-                    <h4>NewsArticles
-    
-                </h4>
-                    {articles.length > 0 &&
-                        articles.map(article => {
-                            return (
-                                <div>
-                                    <a href={article.url} target="_blank">
-                                        {article.name}
-                                    </a>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                </Row>
-            </Container>
+            {articles.length === 0 ?
+                showRecords ? <Spinner animation="border" size="lg" /> : 'No records available'
+                :
+                <Container >
+                    <Row>
+                        <Col md={6}>
+                            <h2>Articles</h2>
+                        </Col>
+                        <Col md={6} className="mt-2">
+                            <input type="text" placeholder="Enter value to filter" onChange={(e) => setArticalFilter(e.target.value)} value={articleFilter} />
+                            <input type="button" value="Filter" onClick={filterArticles} />
+                        </Col>
+                    </Row>
+                    <Row className="mt-2">
+                        {
+                            articles.length > 0 &&
+                            articles.map((article) => {
+                                return (
+                                    <div className="article" key={article.id}>
+                                        <Card>
+                                            <a href={article.url} target="_blank"><Card.Header>{article.name}</Card.Header>
+                                            </a>
+                                            <Card.Body>
+                                                <blockquote className="blockquote mb-0">
+                                                    <p>
+                                                        {article.description}
+                                                    </p>
+                                                </blockquote>
+                                            </Card.Body>
+                                        </Card>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Row>
+                </Container>
             }
         </>
-                
+
     )
 }
 
